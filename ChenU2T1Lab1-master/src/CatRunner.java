20ae6cc56b3de73940cd3b66e8ce10195f426dd9
test <annac101@nycstudents.net>
2022-09-30 01:05:34 -0400

    final?

---
+++ ChenU2T1Lab1-master/src/CatRunner.java
@@ -0,0 +1,11 @@
+public class CatRunner {
+public static void main (String[] args) {
+Cat cat1 = new Cat("Gary",6,11);
+cat1.printCatInfo();
+cat1.introduce();
+
+Cat cat2 = new Cat("Ashe",8,9);
+cat2.printCatInfo();
+cat2.introduce();
+}
+}
